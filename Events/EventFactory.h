f052fb96147bf941e2d545c8af2a666978340b4e
Ori Elkabetz <orielk2@gmail.com>
2025-01-16 18:13:37 +0200

    creating EventFactory.h

---
+++ Events/EventFactory.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "Snail.h"
+#include "Slime.h"
+#include "Balrog.h"
+#include "Pack.h"
+#include "SolarEclipse.h"
+#include "PotionsMerchant.h"
+#include <vector>
+using std::vector;
+
+class EventFactory{
+    private:
+    vector<std::string> eventLineVector;
+    public:
+    EventFactory(vector<std::string> eventLineVector);
+    Event* create();
+};
