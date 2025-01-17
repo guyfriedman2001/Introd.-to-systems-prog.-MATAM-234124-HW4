14ecdbbe5e9281b3ac87037b305f97d3654f5168
guy friedman <guy.friedman2001@gmail.com>
2025-01-17 17:23:35 +0200

    add instructions for MatamPlayer

--- Adapters/MatamPlayer.h
+++ Adapters/MatamPlayer.h
@@ -1,29 +1,19 @@
 #pragma once;
 
 #include "Players/Player.h"
-#include "Jobs/Job.h"
-#include "Charachters/Character.h"
 #include "Adapters/PlayerAdapter.h"
 
 class MatamPlayer : public PlayerAdapter{
 
 
     /**
-     getMaxHealthPoints()
-    make sure that paycoins doesn't get below zero. throw runtime_error if it does.
-    make sure heal doesn't get over MaxHealthPoints
-    forceUp(unsigned int effect )
-    forceDown(unsigned int effect)
-
-
-    win( unsigned int Loot)
-    lose(unsigned int Damage)
-    getMaxHealthPoints()
-    setCoins(int coins) +throw Error if coins below 0
-    setHealthPoints(int healthPoints) + make sure its not over maxHP
-    getJobType() - i saw you did getJob but i dont know how to use in. maybe it should be *Job?
-    forceUp(unsigned int effect )
-    forceDown(unsigned int effect)
+     * Ori - if you want me to add a functionality to player,
+     * write the declaration of the function, and inside the fubction write a
+     * comment describing what you want me to do, and ill add the necessary
+     * calls to player
+     * 
+     * (if you want, you can also add the calls, but if you dont feel like it just do 
+     * what i wrote above)
      */
 
 public:
