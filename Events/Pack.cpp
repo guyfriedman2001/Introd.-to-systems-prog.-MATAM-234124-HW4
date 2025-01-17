efa92e7d26eb1c7a5d62f3a345549ab8126df715
Ori Elkabetz <orielk2@gmail.com>
2025-01-17 16:20:44 +0200

    setting CP for packs in pack

--- Events/Pack.cpp
+++ Events/Pack.cpp
@@ -8,6 +8,17 @@ Pack::Pack(vector<std::string> monstersString): Monster(0,0,0){
     }
     monstersString.erase(monstersString.begin());
     for(int i = monstersString.size(), int j = 0 ; i > 0; i--, j++){
+        //  EventFactory newEvent = EventFactory(monstersString);
+        // if(monstersString[j] == "Pack"){
+        //     packs.push_back(dynamic_cast<Pack*>(newEvent.create()));
+        // }
+        // else{
+        //     monsters.push_back(dynamic_cast<Monster*>(newEvent.create()));
+        // }
+        // Monster::setCombatPower(monsters[j]->getCombatPower());
+        // Monster::setLoot(monsters[j]->getLoot());
+        // Monster::setDamage(monsters[j]->getDamage());
+        // monstersString.erase(monstersString.begin());
         EventFactory newEvent = EventFactory(monstersString);
         monsters.push_back(dynamic_cast<Monster*>(newEvent.create()));
         Monster::setCombatPower(monsters[j]->getCombatPower());
@@ -15,13 +26,32 @@ Pack::Pack(vector<std::string> monstersString): Monster(0,0,0){
         Monster::setDamage(monsters[j]->getDamage());
         monstersString.erase(monstersString.begin());
     }
+
     // if(monsters.size() != packSize) error! ?
 }
 void Pack::startEvent(Player* player){
     Monster::startEvent(player);
+    setNewCombatPower();
 }
 
-std::string Pack::getName() const{
-    int size = monsters.size();
-    return ("Pack of " + std::to_string(size) + " members");
+void Pack::setNewCombatPower(){
+    for(auto iter = monsters.begin(); iter != monsters.end(); iter++){
+        if((*iter)->getName() == "Balrog"){
+            Monster::setCombatPower(2); 
+        }
+        if((*iter)->getName() == "Pack"){
+            int pastCombatPower =  (*iter)->getCombatPower();
+            dynamic_cast<Pack*>(*iter)->setNewCombatPower();
+            Monster::setCombatPower((*iter)->getCombatPower() - pastCombatPower);
+        }
+    }
+}
+
+
+std::string Pack::getName(){
+    return("Pack");
+}
+
+std::string Pack::getDescription(){
+    return(getName() + " of " + std::to_string(monsters.size()) + " members"+ Monster::getDescription());
 }
--- Events/Pack.h
+++ Events/Pack.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Monster.h"
+#include <map>
+using std::map;
 
 class Pack : public Monster{
     private:
     vector<Monster*> monsters;
+    //vector<Pack*> packs;
     public:
     Pack(vector<std::string> monstersString);
     void startEvent(Player* player) override;
-    std::string getName() const override;
+    std::string getName() override;
+    std::string getDescription() override;
+    void setNewCombatPower();
 };
