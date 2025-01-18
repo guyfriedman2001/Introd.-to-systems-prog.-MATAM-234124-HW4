042a0fdad97ede4710a3f1874e1436f6b16d93be
guy friedman <guy.friedman2001@gmail.com>
2025-01-18 17:47:17 +0200

    add documentation Magician.h

--- Players/Magician.h
+++ Players/Magician.h
@@ -6,10 +6,20 @@
 #define MAGICIAN_H
 #include "Magical.h"
 
-
+/**
+ * @brief Represents a Magician job, a subclass of Magical.
+ */
 class Magician : public Magical {
 public:
+
+    /**
+     * @brief Destructor for the Magician class.
+     */
     virtual ~Magician() = default;
+
+    /**
+     * @brief Default constructor for the Magician class.
+     */
     Magician() : Magical() {}
 };
 
