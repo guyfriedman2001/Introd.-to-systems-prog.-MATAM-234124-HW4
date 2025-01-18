4bb5fcad72f6184422d049982fc2a7c61bdaaeee
guy friedman <guy.friedman2001@gmail.com>
2025-01-18 13:48:44 +0200

    Update RiskTaking.h

--- Charachters/RiskTaking.h
+++ Charachters/RiskTaking.h
@@ -5,10 +5,31 @@
 #ifndef RISKTAKING_H
 #define RISKTAKING_H
 #include "Character.h"
+#include "Player.h"
+using std::string;
 
 
 
 class RiskTaking : public Character {
+
+    RiskTaking() = default;
+
+    RiskTaking(Player* player) : Character(player) {}
+
+    virtual ~RiskTaking() = default;
+
+    virtual string getClassName() override {
+        return "RiskTaking";
+    }
+
+    virtual void tradeMerchant(int price, int healingPower) override {
+        Player* player = this->player;
+        if (player->canPurchase(price)&&(!player->isFullHealth())){
+            player->payCoins(price);
+            player->heal(healingPower);
+        }
+    }
+
 
 };
 
