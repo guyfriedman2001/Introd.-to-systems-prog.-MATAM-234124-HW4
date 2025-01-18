a9727cf44a13ca2e705659fc5267e168265b5204
guy friedman <guy.friedman2001@gmail.com>
2025-01-18 17:48:52 +0200

    add documentation Warrior.h

--- Players/Warrior.h
+++ Players/Warrior.h
@@ -6,18 +6,31 @@
 #define WARRIOR_H
 #include "Strong.h"
 
-
+/**
+ * @brief Represents the Warrior job class, inheriting from the Strong base class.
+ */
 class Warrior : public Strong {
 
 public:
 
+    /**
+     * @brief Destructor for the Warrior class.
+     */
     virtual ~Warrior() = default;
 
+    /**
+     * @brief Default constructor for the Warrior class.
+     */
     Warrior() : Strong() {
         this->MAX_HEALTH = 150;
         this->currentHealth = 150;
     }
 
+    /**
+     * @brief Calculates the Warrior's combat power.
+     * 
+     * @return The combat power, calculated as 2 times the force plus the level of the Warrior.
+     */
     unsigned int getCombatPower() const override {
         return (this->getForce()*2) + this->getLevel();
     }
