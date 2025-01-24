18fadc9a3984f2e776218ec680086c6f533717ef
guy friedman <guy.friedman2001@gmail.com>
2025-01-24 21:25:06 +0200

    move code from cpp to .h

--- Players/Warrior.cpp
+++ Players/Warrior.cpp
@@ -2,3 +2,12 @@
 string Warrior::getDescription() const{
     return "Warrior";
 }
+
+Warrior::Warrior() : Strong() {
+    this->MAX_HEALTH = 150;
+    this->currentHealth = 150;
+}
+
+int Warrior::getCombatPower() const {
+    return ((this->getForce()*2) + this->getLevel());
+}
--- Players/Warrior.h
+++ Players/Warrior.h
@@ -18,19 +18,20 @@ public:
     /**
      * @brief Default constructor for the Warrior class.
      */
-    Warrior() : Strong() {
-        this->MAX_HEALTH = 150;
-        this->currentHealth = 150;
-    }
+    Warrior();
 
     /**
      * @brief Calculates the Warrior's combat power.
      * 
      * @return The combat power, calculated as 2 times the force plus the level of the Warrior.
      */
-    int getCombatPower() const override {
-        return ((this->getForce()*2) + this->getLevel());
-    }
+    int getCombatPower() const override;
+
+    /**
+     * @brief Retrieves the description of the Warrior class.
+     * 
+     * @return A string representing the Warrior's description.
+     */
     virtual string getDescription() const override;
 
 };
