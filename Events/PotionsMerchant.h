0c1aa00e0fc997e53446b5fe6dd212c316528405
Ori Elkabetz <orielk2@gmail.com>
2025-01-24 18:13:49 +0200

    removing magic numbers from PM

--- Events/PotionsMerchant.h
+++ Events/PotionsMerchant.h
@@ -3,7 +3,9 @@
 #define POTIONSMERCHANT_H
 class PotionsMerchant : public SpecialEvent{
     private:
-    int coins;
+    static const int MAX_HP_FOR_RISKTAKEING = 50;
+    static const int  HEAL_POINTS = 10;
+    static const int COINS = 5;
     void makeDeal(Player& player);
     public:
     string startEvent(Player& player) override;
